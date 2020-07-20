12aa9f7cebae61b0c723613a039d60c8ff10f09d
Sravan Suhas <sravansuhas123@gmail.com>
2020-07-20 15:06:11 +0530

    Example

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -5,13 +5,20 @@ const inlineAction = require("./actions/inline");
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.start((ctx) => {
+/* bot.start((ctx) => {
     return startAction(ctx);
 });
 
 bot.on("inline_query", (ctx) => {
     return inlineAction(ctx);
 });
+ */
+
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on("sticker", (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.launch();
 
 exports.handler = async(event) => {
     try {
